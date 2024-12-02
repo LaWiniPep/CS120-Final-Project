700ea8f22d2334072aa37ac11bfeb954016f058c
LaWiniPep <alexmuller@ameritech.net>
2024-12-02 18:06:46 -0500

    Add files via upload

--- Final Project/player_working.py
+++ Final Project/player_working.py
@@ -13,30 +13,45 @@ class Player(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
         self.colorRect("red", (20, 20))
-        self.moveSpeed = 2
+        self.moveSpeed = 5
         self.tileOver = (0,0)
         self.tileState = 0
         
     def process(self):
-        
+
         if self.isKeyPressed(pygame.K_UP):
             self.y -= self.moveSpeed
+
+            
         if self.isKeyPressed(pygame.K_DOWN):
             self.y += self.moveSpeed
+
+            
         if self.isKeyPressed(pygame.K_LEFT):
             self.x -= self.moveSpeed
+
+            
         if self.isKeyPressed(pygame.K_RIGHT):
             self.x += self.moveSpeed
             
-        
+  
             
         #Speed on different tiles
         if self.tileState == 0:
-            self.moveSpeed = 1.5
+            self.moveSpeed = 1
         elif self.tileState == 1:
-            self.moveSpeed = 3
+            self.moveSpeed = 2
         elif self.tileState == 2:
             self.moveSpeed = .2
+        elif self.tileState == 9:
+            self.y += 2
+        elif self.tileState == 10:
+            self.y -= 2
+        elif self.tileState == 11:
+            self.x -= 2
+        elif self.tileState == 12:
+            self.x += 2
+        
         else:
             self.moveSpeed = 2
 #             
